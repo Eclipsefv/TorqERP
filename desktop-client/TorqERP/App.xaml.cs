aab0cbd8c38f1b2651d4220c4f65710a9ffd2228
Eclipsefv <aaronfv4005@gmail.com>
2026-01-28 10:33:43 +0100

    First frontend MAUI Blazor commit

---
+++ desktop-client/TorqERP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TorqERP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
